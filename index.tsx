a740995c6950a2a83b34c40264388a29c580eb26
Ashraf Morningstar <ashrafmorningstar@gmail.com>
2025-12-16 14:36:43 +0530

    feat: Final Premium Polish & SEO

---
+++ index.tsx
@@ -0,0 +1,28 @@
+/**
+ * @file index.tsx
+ * @author Ashraf Morningstar <https://github.com/AshrafMorningstar>
+ * @copyright 2025 Ashraf Morningstar
+ * @license MIT
+ *
+ * 🌌 MSFolio - The Ultimate Microsoft-Style Portfolio
+ * "The future is unwritten, but the code is compiled."
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { ErrorBoundary } from './components/common/ErrorBoundary';
+
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+  throw new Error("Could not find root element to mount to");
+}
+
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
+  </React.StrictMode>
+);
